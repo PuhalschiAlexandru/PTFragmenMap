a93bfdd3479863ec75aaad641797fd26287d1e1b
Alex Puhalschi <a.puhalschi@yahoo.com>
2017-07-03 12:25:54 +0300

    Added google map position to cluj

--- app/src/main/java/com/tapptitude/fragmentmapapp/FragmentMapScreen.java
+++ app/src/main/java/com/tapptitude/fragmentmapapp/FragmentMapScreen.java
@@ -1,6 +1,7 @@
 package com.tapptitude.fragmentmapapp;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,16 +10,16 @@ import android.view.ViewGroup;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import butterknife.BindView;
 
 /**
  * Created by alexpuhalschi on 03/07/2017.
  */
 
 public class FragmentMapScreen extends Fragment implements OnMapReadyCallback {
-    private  SupportMapFragment mMapFragment;
+    SupportMapFragment mMapFragment;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -28,12 +29,13 @@ public class FragmentMapScreen extends Fragment implements OnMapReadyCallback {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mMapFragment = (SupportMapFragment)getFragmentManager().findFragmentById(R.id.fms_f_map_fragment);
-        mMapFragment.getMapAsync(this);
+        mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.fms_f_map_fragment);
+        mMapFragment.getMapAsync(FragmentMapScreen.this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions().anchor(0.5f, 0.5f));
+        LatLng mClujLocation = new LatLng(46.7667, 23.6);
+        googleMap.addMarker(new MarkerOptions().anchor(0.5f, 0.5f).position(mClujLocation));
     }
 }
