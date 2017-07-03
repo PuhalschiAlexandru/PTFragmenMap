3143a8c613650fc6a931b9fd5a7493b9977fb91e
Alex Puhalschi <a.puhalschi@yahoo.com>
2017-07-03 11:16:23 +0300

    implemented google map methods

--- app/src/main/java/com/tapptitude/fragmentmapapp/FragmentMapScreen.java
+++ app/src/main/java/com/tapptitude/fragmentmapapp/FragmentMapScreen.java
@@ -1,21 +1,39 @@
 package com.tapptitude.fragmentmapapp;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import butterknife.BindView;
+
 /**
  * Created by alexpuhalschi on 03/07/2017.
  */
 
-public class FragmentMapScreen extends Fragment {
+public class FragmentMapScreen extends Fragment implements OnMapReadyCallback {
+    @BindView(R.id.fms_f_map_fragment)
+    SupportMapFragment mMapFragment;
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_map_screen, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions().anchor(0.5f, 0.5f));
     }
 }
