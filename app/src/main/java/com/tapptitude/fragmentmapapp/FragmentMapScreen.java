95bbc6c08cce21c7eff4640ae383321730a642bc
Alex Puhalschi <a.puhalschi@yahoo.com>
2017-07-03 10:34:19 +0300

    Added map api key in android manifest, added google map dependency, created fragment and map activity

---
+++ app/src/main/java/com/tapptitude/fragmentmapapp/FragmentMapScreen.java
@@ -0,0 +1,21 @@
+package com.tapptitude.fragmentmapapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by alexpuhalschi on 03/07/2017.
+ */
+
+public class FragmentMapScreen extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map_screen, container, false);
+    }
+}
